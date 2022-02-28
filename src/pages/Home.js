69f927c60ff1acbdf399d0beeed711679783dd8d
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2022-02-28 17:32:45 -0500

    fetch github API and get users

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import UserResults from "../components/users/UserResults";
+
+const Home = () => {
+  return (
+    <>
+      {/* SEARCH USERS */}
+      <UserResults />
+    </>
+  );
+};
+
+export default Home;
