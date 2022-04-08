672b1f2a9605b151be321ffaa7e0b4ad3fb29180
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2022-04-07 20:39:29 -0400

    About Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1 className="text-6xl mb-4">GitHub Finder</h1>
-      <p className="mb-4 text-2xl font-light">
+    <div className="text-stone-900">
+      <h1 className="text-stone-900 text-6xl mb-4">GitHub Finder</h1>
+      <p className="text-stone-900 mb-4 text-2xl font-light">
         A React app to search GitHub profiles and see profile details. This
         project is part of the
         <a
@@ -21,11 +21,11 @@ const About = () => {
         .
       </p>
       <p className="text-lg text-gray-400">
-        Version <span className="text-white">1.0.0</span>
+        Version <span className="text-black">1.0.0</span>
       </p>
       <p className="text-lg text-gray-400">
         Layout By:
-        <a className="text-white">Linh Bui</a>
+        <a className="text-black">Linh Bui</a>
       </p>
     </div>
   );
